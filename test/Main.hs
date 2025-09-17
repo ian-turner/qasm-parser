b2f2540c47858eb0d0eb9458af60ea5cb72ba534
Ian Turner <i.r.turner55@gmail.com>
2025-09-17 09:40:37 -0400

    added test file

--- test/Main.hs
+++ test/Main.hs
@@ -5,7 +5,7 @@ import Parser
 
 main :: IO ()
 main = do
-    (Program _ stmts) <- parseQasmFile "tmp/adder10_inlined.qasm"
+    (Program _ stmts) <- parseQasmFile "test/adder10_inlined.qasm"
     let stmts_str = map show stmts
     putStr (unlines stmts_str)
     return ()
